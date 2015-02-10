614a9d7baf9f7b629572fac8d8356056c0c1caeb
Nepetalactone <tobias.meusburger@gmx.net>
2015-02-10 21:48:00 +0100

    Change time to 20/5

--- Stretch!/Stretch!/MainWindow.xaml.cs
+++ Stretch!/Stretch!/MainWindow.xaml.cs
@@ -16,7 +16,7 @@ namespace Stretch_
             _icon.Icon = Properties.Resources.icon;
             InitializeComponent();
             
-            Timer timer = new Timer(2100000);
+            Timer timer = new Timer(1500000);
             timer.Elapsed += _timer_Elapsed;
             timer.Start();
         }
