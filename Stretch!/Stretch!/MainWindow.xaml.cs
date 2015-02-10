f34bf501ce24cf85c4240d1a2007995300fea632
Nepetalactone <tobias.meusburger@gmx.net>
2015-02-10 17:34:52 +0100

    initial solution

---
+++ Stretch!/Stretch!/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Timers;
+using System.Windows;
+using Hardcodet.Wpf.TaskbarNotification;
+
+namespace Stretch_
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly TaskbarIcon _icon;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _icon = new TaskbarIcon();
+
+            Timer timer = new Timer(300000);
+            timer.Elapsed += _timer_Elapsed;
+            timer.Start();
+        }
+
+        void _timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            _icon.ShowBalloonTip("Stretch!", "Take a break", BalloonIcon.Info);
+        }
+    }
+}
